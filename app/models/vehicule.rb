605e058f2842da060fd5109f6f76bd9bf888a22b
Muriel Oger <muriel.oger@gmail.com>
2019-11-19 11:04:32 +0100

    model Vehicule done with validation

--- app/models/vehicule.rb
+++ app/models/vehicule.rb
@@ -1,3 +1,14 @@
 class Vehicule < ApplicationRecord
   belongs_to :user
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :category, presence: true, inclusion: { in: ["JetPack", "Catapulte", "X-wing","Fusée", "Soucoupe", "Canon"]}
+  validates :capacity, presence: true
+  validates :country, presence: true
+  validates :city, presence: true
+  validates :address, presence: true
+  validates :daily_price, presence: true
+  validates :permit, presence: true
+  validates :availability, presence: true
+
 end
