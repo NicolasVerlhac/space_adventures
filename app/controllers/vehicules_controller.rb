1c631d3be449a61836f183e71d9ea5517090f40b
Muriel Oger <muriel.oger@gmail.com>
2019-11-20 12:54:53 +0100

    pundit en cours

--- app/controllers/vehicules_controller.rb
+++ app/controllers/vehicules_controller.rb
@@ -1,6 +1,8 @@
 class VehiculesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show]
+
   def index
+    @vehicules = policy_scope(Vehicule).order(created_at: :desc)
     if params[:query].present?
       @vehicules = Vehicule.where(category: params[:query])
     else
@@ -12,14 +14,17 @@ class VehiculesController < ApplicationController
     # @vehicule = Vehicule.find_by(id: params[:id])
     # @vehicule = Vehicule.where(id: params[:id])
     @vehicule = Vehicule.find_by_id(params[:id])
+    authorize(@vehicule)
   end
 
   def new
     @vehicule = Vehicule.new
+    authorize(@vehicule)
   end
 
   def create
     @vehicule = Vehicule.new(vehicule_params)
+    authorize(@vehicule)
     @vehicule.user = current_user
     if @vehicule.save
       redirect_to vehicule_path(@vehicule)
